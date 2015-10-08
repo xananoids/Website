9594a74b67f3120d763f34c6218793897416eb8d
HVish <vishnusingh1729@gmail.com>
2015-10-08 11:58:19 +0530

    updated preloader

--- js/preloader_canvas.js
+++ js/preloader_canvas.js
@@ -1,37 +1,43 @@
-var cSpeed=9;
-	var cWidth=220;
-	var cHeight=19;
-	var cTotalFrames=22;
-	var cFrameWidth=220;
-	var cImageSrc='images/sprites.gif';
-	
-	var cImageTimeout=false;
-	
-	function startAnimation(){
-		
-		document.getElementById('loaderImage').innerHTML='<canvas id="canvas" width="'+cWidth+'" height="'+cHeight+'"><p>Your browser does not support the canvas element.</p></canvas>';
-		
-		//FPS = Math.round(100/(maxSpeed+2-speed));
-		FPS = Math.round(100/cSpeed);
-		SECONDS_BETWEEN_FRAMES = 1 / FPS;
-		g_GameObjectManager = null;
-		g_run=genImage;
+/*var cSpeed=50;
+var cWidth=816;
+var cHeight=620;
+var cTotalFrames=22;
+var cFrameWidth=816;
+var cImageSrc='images/xananoids-gears-animated.gif';
 
-		g_run.width=cTotalFrames*cFrameWidth;
-		genImage.onload=function (){cImageTimeout=setTimeout(fun, 0)};
-		initCanvas();
-	}
+var cImageTimeout=false;
+
+function startAnimation(){
 	
+	document.getElementById('loaderImage').innerHTML='<canvas id="canvas" width="'+cWidth+'" height="'+cHeight+'"><p>Your browser does not support the canvas element.</p></canvas>';
 	
-	function imageLoader(s, fun)//Pre-loads the sprites image
-	{
-		clearTimeout(cImageTimeout);
-		cImageTimeout=0;
-		genImage = new Image();
-		genImage.onload=function (){cImageTimeout=setTimeout(fun, 0)};
-		genImage.onerror=new Function('alert(\'Could not load the image\')');
-		genImage.src=s;
-	}
-	
-	//The following code starts the animation
-	new imageLoader(cImageSrc, 'startAnimation()');
+	//FPS = Math.round(100/(maxSpeed+2-speed));
+	FPS = Math.round(100/cSpeed);
+	SECONDS_BETWEEN_FRAMES = 1 / FPS;
+	g_GameObjectManager = null;
+	g_run=genImage;
+
+	g_run.width=cTotalFrames*cFrameWidth;
+	genImage.onload=function (){cImageTimeout=setTimeout(fun, 0)};
+	initCanvas();
+}
+
+
+function imageLoader(s, fun)//Pre-loads the sprites image
+{
+	clearTimeout(cImageTimeout);
+	cImageTimeout=0;
+	genImage = new Image();
+	genImage.onload=function (){cImageTimeout=setTimeout(fun, 0)};
+	genImage.onerror=new Function('alert(\'Could not load the image\')');
+	genImage.src=s;
+}
+
+//The following code starts the animation
+new imageLoader(cImageSrc, 'startAnimation()');*/
+
+function preloader(){
+    document.getElementById("loaderImage").style.display = "none";
+    document.getElementById("cnr").style.display = "block";
+}//preloader
+window.onload = preloader;
